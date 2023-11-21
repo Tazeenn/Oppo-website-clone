8a4a54699f3dfb819c2cfd1303c8908d6f8c8abd
Tazeen Soudagar <95432172+Tazeenn@users.noreply.github.com>
2023-11-21 17:46:28 +0530

    Initial commit

---
+++ nav-links/smartphones/smart.js
@@ -0,0 +1,562 @@
+function newImage(){
+    var image = document.getElementById("tablet")
+    image.src='/images/smartphones/1st-div-img2.png'
+    var brder = document.getElementById("no-border")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there")
+    brder.style.border = '1px solid black'
+}
+
+function oldImage(){
+    var image = document.getElementById("tablet")
+    image.src = "/images/smartphones/1st-div-img.png"            
+    var brder = document.getElementById("no-border")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("border-there")
+    brder.style.border = 'none'
+}
+
+// 2nd div
+
+function newImage2div(){
+    var image = document.getElementById("div2-tablet")
+    image.src='/images/smartphones/2nd-div-img3.jpg'
+    var brder = document.getElementById("div2-no-border")
+    brder.style.border = 'none'
+    var brder = document.getElementById("div2-border-there")
+    brder.style.border = '1px solid black'
+}
+
+function oldImage2div(){
+    var image = document.getElementById("div2-tablet")
+    image.src = "/images/smartphones/2nd-div-img2.jpg"            
+    var brder = document.getElementById("div2-no-border")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("div2-border-there")
+    brder.style.border = 'none'
+    console.log(image);
+}
+
+
+// reno script
+
+// 1st 
+
+function newImageReno(){
+    var image = document.getElementById("reno1")
+    image.src='/images/smartphones/reno-img-12.png'
+    var brder = document.getElementById("no-border-reno")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-reno")
+    brder.style.border = '1px solid black'
+}
+
+function oldImageReno(){
+    var image = document.getElementById("reno1")
+    image.src = "/images/smartphones/reno-img-1.png"            
+    var brder = document.getElementById("no-border-reno")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("border-there-reno")
+    brder.style.border = 'none'
+    console.log(image);
+}
+
+// 2nd
+
+function newImageReno2(){
+    var image = document.getElementById("reno2")
+    image.src='/images/smartphones/reno-img-22.png'
+    var brder = document.getElementById("no-border-reno2")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-reno2")
+    brder.style.border = '1px solid black'
+}
+
+function oldImageReno2(){
+    var image = document.getElementById("reno2")
+    image.src = "/images/smartphones/reno-img-2.png"            
+    var brder = document.getElementById("no-border-reno2")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("border-there-reno2")
+    brder.style.border = 'none'
+    console.log(image);
+}
+
+// 3rd
+
+function newImageReno3(){
+    var image = document.getElementById("reno3")
+    image.src='/images/smartphones/reno-img-32.png'
+    var brder = document.getElementById("no-border-reno3")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-reno3")
+    brder.style.border = '1px solid black'
+}
+
+function oldImageReno3(){
+    var image = document.getElementById("reno3")
+    image.src = "/images/smartphones/reno-img-3.png"            
+    var brder = document.getElementById("no-border-reno3")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("border-there-reno3")
+    brder.style.border = 'none'
+    console.log(image);
+}
+
+// 4
+
+function newImageReno4(){
+    var image = document.getElementById("reno4")
+    image.src='/images/smartphones/reno-img-42.png'
+    var brder = document.getElementById("no-border-reno4")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-reno4")
+    brder.style.border = '1px solid black'
+}
+
+function oldImageReno4(){
+    var image = document.getElementById("reno4")
+    image.src = "/images/smartphones/reno-img-4.png"            
+    var brder = document.getElementById("no-border-reno4")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("border-there-reno4")
+    brder.style.border = 'none'
+    console.log(image);
+}
+
+
+
+// 6th
+
+function newImageReno6(){
+    var image = document.getElementById("reno6")
+    image.src='/images/smartphones/reno-img-62.png'
+    var brder = document.getElementById("no-border-reno6")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-reno6")
+    brder.style.border = '1px solid black'
+}
+
+function new2ImageReno6(){
+    var image = document.getElementById("reno6")
+    image.src='/images/smartphones/reno-img-63.png'
+    var brder = document.getElementById("no-border-reno6")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-reno6")
+    brder.style.border = '1px solid black'
+}
+
+function oldImageReno6(){
+    var image = document.getElementById("reno6")
+    image.src = "/images/smartphones/reno-img-61.png"            
+    var brder = document.getElementById("no-border-reno6")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("border-there-reno6")
+    brder.style.border = 'none'
+    console.log(image);
+}
+
+// 7th
+
+function newImageReno7(){
+    var image = document.getElementById("reno7")
+    image.src='/images/smartphones/reno-img-72.png'
+    var brder = document.getElementById("no-border-reno7")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-reno7")
+    brder.style.border = '1px solid black'
+}
+
+function oldImageReno7(){
+    var image = document.getElementById("reno7")
+    image.src = "/images/smartphones/reno-img-7.png"            
+    var brder = document.getElementById("no-border-reno7")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("border-there-reno7")
+    brder.style.border = 'none'
+    console.log(image);
+}
+
+// f series script
+
+//1st
+
+function newImagef1(){
+    var image = document.getElementById("f1")
+    image.src='/images/smartphones/f-series/img-12.png'
+    var brder = document.getElementById("no-border-f1")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-f1")
+    brder.style.border = '1px solid black'
+}
+
+function oldImagef1(){
+    var image = document.getElementById("f1")
+    image.src = "/images/smartphones/f-series/img-1.png"            
+    var brder = document.getElementById("no-border-f1")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("border-there-f1")
+    brder.style.border = 'none'
+    console.log(image);
+}
+
+//2nd
+
+function newImagef2(){
+    var image = document.getElementById("f2")
+    image.src='/images/smartphones/f-series/img-22.png'
+    var brder = document.getElementById("no-border-f2")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-f2")
+    brder.style.border = '1px solid black'
+}
+
+function oldImagef2(){
+    var image = document.getElementById("f2")
+    image.src = "/images/smartphones/f-series/img-2.png"            
+    var brder = document.getElementById("no-border-f2")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("border-there-f2")
+    brder.style.border = 'none'
+    console.log(image);
+}
+
+//3rd
+
+function newImagef3(){
+    var image = document.getElementById("f3")
+    image.src='/images/smartphones/f-series/img-3.png'
+    var brder = document.getElementById("no-border-f3")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-f3")
+    brder.style.border = '1px solid black'
+}
+
+function oldImagef3(){
+    var image = document.getElementById("f3")
+    image.src = "/images/smartphones/f-series/img-32.png"            
+    var brder = document.getElementById("no-border-f3")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("border-there-f3")
+    brder.style.border = 'none'
+    console.log(image);
+}
+
+//5th
+
+function newImagef5(){
+    var image = document.getElementById("f5")
+    image.src='/images/smartphones/f-series/img-5.png'
+    var brder = document.getElementById("no-border-f5")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-f5")
+    brder.style.border = '1px solid black'
+}
+
+function oldImagef5(){
+    var image = document.getElementById("f5")
+    image.src = "/images/smartphones/f-series/img-52.png"            
+    var brder = document.getElementById("no-border-f5")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("border-there-f5")
+    brder.style.border = 'none'
+    console.log(image);
+}
+
+
+//6th
+
+function newImagef6(){
+    var image = document.getElementById("f6")
+    image.src='/images/smartphones/f-series/img-62.png'
+    var brder = document.getElementById("no-border-f6")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-f6")
+    brder.style.border = '1px solid black'
+}
+
+function new2Imagef6(){
+    var image = document.getElementById("f6")
+    image.src='/images/smartphones/f-series/img-63.png'
+    var brder = document.getElementById("no-border-f6")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-f6")
+    brder.style.border = '1px solid black'
+}
+
+function oldImagef6(){
+    var image = document.getElementById("f6")
+    image.src = "/images/smartphones/f-series/img-61.png"            
+    var brder = document.getElementById("no-border-f6")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("border-there-f6")
+    brder.style.border = 'none'
+    console.log(image);
+}
+
+//7th
+
+function newImagef7(){
+    var image = document.getElementById("f7")
+    image.src='/images/smartphones/f-series/img-71.png'
+    var brder = document.getElementById("no-border-f7")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-f7")
+    brder.style.border = '1px solid black'
+}
+
+function oldImagef7(){
+    var image = document.getElementById("f7")
+    image.src = "/images/smartphones/f-series/img-7.png"            
+    var brder = document.getElementById("no-border-f7")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("border-there-f7")
+    brder.style.border = 'none'
+    console.log(image);
+}
+
+//8th
+
+
+function newImagef8(){
+    var image = document.getElementById("f8")
+    image.src='/images/smartphones/f-series/img-82.png'
+    var brder = document.getElementById("no-border-f8")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-f8")
+    brder.style.border = '1px solid black'
+}
+
+function new2Imagef8(){
+    var image = document.getElementById("f8")
+    image.src='/images/smartphones/f-series/img-83.png'
+    var brder = document.getElementById("no-border-f8")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-f8")
+    brder.style.border = '1px solid black'
+}
+
+function oldImagef8(){
+    var image = document.getElementById("f8")
+    image.src = "/images/smartphones/f-series/img-81.png"            
+    var brder = document.getElementById("no-border-f8")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("border-there-f8")
+    brder.style.border = 'none'
+    console.log(image);
+}
+
+
+// a series starts
+
+//1st
+function newImageA1(){
+    var image = document.getElementById("A1")
+    image.src='/images/smartphones/A-series/img-12.png'
+    var brder = document.getElementById("no-border-A1")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-A1")
+    brder.style.border = '1px solid black'
+}
+
+function oldImageA1(){
+    var image = document.getElementById("A1")
+    image.src = "/images/smartphones/A-series/img-1.png"            
+    var brder = document.getElementById("no-border-A1")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("border-there-A1")
+    brder.style.border = 'none'
+    console.log(image);
+}
+
+//2nd
+
+function newImagea2(){
+    var image = document.getElementById("a2")
+    image.src='/images/smartphones/A-series/img-21.png'
+    var brder = document.getElementById("no-border-a2")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there2-a2")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-a2")
+    brder.style.border = '1px solid black'
+}
+
+function new2Imagea2(){
+    var image = document.getElementById("a2")
+    image.src='/images/smartphones/A-series/img-22.png'
+    var brder = document.getElementById("no-border-a2")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-a2")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there2-a2")
+    brder.style.border = '1px solid black'
+}
+
+function oldImagea2(){
+    var image = document.getElementById("a2")
+    image.src = "/images/smartphones/A-series/img-2.png"            
+    var brder = document.getElementById("no-border-a2")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("border-there-a2")
+    brder.style.border = 'none'
+    console.log(image);
+    var brder = document.getElementById("border-there2-a2")
+    brder.style.border = 'none'
+}
+
+//3rd
+
+function newImagea3(){
+    var image = document.getElementById("a3")
+    image.src='/images/smartphones/A-series/img-31.png'
+    var brder = document.getElementById("no-border-a3")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-a3")
+    brder.style.border = '1px solid black'
+}
+
+function oldImagea3(){
+    var image = document.getElementById("a3")
+    image.src = "/images/smartphones/A-series/img-3.png"            
+    var brder = document.getElementById("no-border-a3")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("border-there-a3")
+    brder.style.border = 'none'
+    console.log(image);
+}
+
+//4th
+
+function newImagea4(){
+    var image = document.getElementById("a4")
+    image.src='/images/smartphones/A-series/img-41.png'
+    var brder = document.getElementById("no-border-a4")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there2-a4")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-a4")
+    brder.style.border = '1px solid black'
+}
+
+function new2Imagea4(){
+    var image = document.getElementById("a4")
+    image.src='/images/smartphones/A-series/img-42.png'
+    var brder = document.getElementById("no-border-a4")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-a4")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there2-a4")
+    brder.style.border = '1px solid black'
+}
+
+function oldImagea4(){
+    var image = document.getElementById("a4")
+    image.src = "/images/smartphones/A-series/img-4.png"            
+    var brder = document.getElementById("no-border-a4")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("border-there-a4")
+    brder.style.border = 'none'
+    console.log(image);
+    var brder = document.getElementById("border-there2-a4")
+    brder.style.border = 'none'
+}
+
+
+//5th
+
+function newImagea5(){
+    var image = document.getElementById("a5")
+    image.src='/images/smartphones/A-series/img-51.png'
+    var brder = document.getElementById("no-border-a5")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-a5")
+    brder.style.border = '1px solid black'
+}
+
+function oldImagea5(){
+    var image = document.getElementById("a5")
+    image.src = "/images/smartphones/A-series/img-5.png"            
+    var brder = document.getElementById("no-border-a5")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("border-there-a5")
+    brder.style.border = 'none'
+    console.log(image);
+}
+
+//7th
+
+function newImagea7(){
+    var image = document.getElementById("a7")
+    image.src='/images/smartphones/A-series/img-71.png'
+    var brder = document.getElementById("no-border-a7")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-a7")
+    brder.style.border = '1px solid black'
+}
+
+function oldImagea7(){
+    var image = document.getElementById("a7")
+    image.src = "/images/smartphones/A-series/img-7.png"            
+    var brder = document.getElementById("no-border-a7")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("border-there-a7")
+    brder.style.border = 'none'
+    console.log(image);
+}
+
+//8th
+function newImagea8(){
+    var image = document.getElementById("a8")
+    image.src='/images/smartphones/A-series/img-81.png'
+    var brder = document.getElementById("no-border-a8")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-a8")
+    brder.style.border = '1px solid black'
+}
+
+function oldImagea8(){
+    var image = document.getElementById("a8")
+    image.src = "/images/smartphones/A-series/img-8.png"            
+    var brder = document.getElementById("no-border-a8")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("border-there-a8")
+    brder.style.border = 'none'
+    console.log(image);
+}
+
+
+// displaying the section 4
+
+function displaySectionView(){
+    var display = document.getElementById("table-img")
+    display.style.display = 'flex'
+    display.style.transition = 'All .2s ease-in-out'
+    var display2 = document.getElementById("insideTablet")
+    display2.style.display = 'flex'
+    display.style.transition = 'All .2s ease-in-out'
+    var button = document.getElementById("butt-view")
+    button.style.display = 'none'
+}
+
+
+// k series style here
+
+function newImagek2(){
+    var image = document.getElementById("k2")
+    image.src='/images/smartphones/k-series/img-21.png'
+    var brder = document.getElementById("no-border-k2")
+    brder.style.border = 'none'
+    var brder = document.getElementById("border-there-k2")
+    brder.style.border = '1px solid black'
+}
+
+function oldImagek2(){
+    var image = document.getElementById("k2")
+    image.src = "/images/smartphones/k-series/img-2.png"            
+    var brder = document.getElementById("no-border-k2")
+    brder.style.border = '1px solid black'
+    var brder = document.getElementById("border-there-k2")
+    brder.style.border = 'none'
+    console.log(image);
+}
